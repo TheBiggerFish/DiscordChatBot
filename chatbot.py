5f32a7f75c66a2c1c7ea9ef10e222dd1364a99b9
TheBiggerFish <clhnumber4@gmail.com>
2021-08-17 09:33:51 -0400

    Use syslog for logging

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,13 @@
 import os
+import logging,logging.handlers
 
 import discord
-from dotenv import load_dotenv
 from discord.ext.commands import Bot
-import time, socket
 
+import time, socket
+from dotenv import load_dotenv
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
+from dateutil import relativedelta
 
 
 load_dotenv()
@@ -17,12 +18,14 @@ V_CHANNEL = int(os.getenv('DISCORD_VOICE_CHANNEL'))
 ROLE = int(os.getenv('DISCORD_ROLE'))
 
 client = discord.Client()
-
+logger = logging.Logger('[DISCORD_CHAT_BOT]')
+handler = logging.handlers.SysLogHandler(address="/dev/log")
+logger.addHandler(handler)
 
 @client.event
 async def on_ready():
     guild = discord.utils.find(lambda g: g.id==GUILD, client.guilds)
-    print(f'{client.user} is connected to {guild.name}\n')
+    logger.info(f'{client.user} is connected to {guild.name}\n')
 
 def is_join(before:discord.VoiceState,after:discord.VoiceState) -> bool:
     return before.channel is None and after.channel is not None
@@ -33,25 +36,25 @@ def is_leave(before:discord.VoiceState,after:discord.VoiceState) -> bool:
 async def clear_chat():
     text_channel = client.get_channel(T_CHANNEL)
     if text_channel.last_message_id is None:
-        print('No messages to delete from empty chat channel')
+        logger.info('No messages to delete from empty chat channel')
         return
     else:
-        print('Deleting messages from empty chat channel')
+        logger.info('Deleting messages from empty chat channel')
     dt = datetime.now() - relativedelta(weeks=2)
     while messages := await text_channel.history(after=dt).flatten():
         await text_channel.delete_messages(messages)
     async for message in text_channel.history(): 
         await message.delete()
-    print('All messages deleted')
+    logger.info('All messages deleted')
 
 @client.event
 async def on_voice_state_update(member:discord.Member, before:discord.VoiceState, after:discord.VoiceState):
     role = client.get_guild(GUILD).get_role(ROLE)
     if is_join(before,after) and after.channel.id==V_CHANNEL:
-        print(member.display_name,"has joined",after.channel.name)
+        logger.info(member.display_name,"has joined",after.channel.name)
         await member.add_roles(role)
     elif is_leave(before,after) and before.channel.id==V_CHANNEL:
-        print(member.display_name,"has left",before.channel.name)
+        logger.info(member.display_name,"has left",before.channel.name)
         await member.remove_roles(role)
         if len(before.channel.voice_states.keys()) == 0:
             await clear_chat()
@@ -62,11 +65,11 @@ def connected():
         socket.create_connection(("8.8.8.8", 53))
         return True
     except Exception as err:
-        print("Error: {0}".format(err))
+        logger.error("Error connecting: {0}".format(err))
         return False
 
 while not connected():
-    print("Failed to connect, waiting 60 seconds")
+    logger.error("Failed to connect, waiting 60 seconds")
     time.sleep(60)
     
 client.run(TOKEN)
