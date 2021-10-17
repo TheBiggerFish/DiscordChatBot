7f9cac6274cbd06cc95afab14e5e24dbafcc893b
TheBiggerFish <clhnumber4@gmail.com>
2021-10-17 15:28:26 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='discordChatBot-TheBiggerFish',
+    version='1.0.0',
+    author='TheBiggerFish',
+    author_email='clhnumber4@gmail.com',
+    description='A discord bot to add a role to user while user is in voice channel',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/TheBiggerFish/DiscordChatBot',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
