b8ac19fec0201bea3448823324296e7e629172f4
TheBiggerFish <clhnumber4@gmail.com>
2021-10-17 09:50:18 -0400

    Created exceptions.py Exceptions to be used in discord object lookup

---
+++ classes/exceptions.py
@@ -0,0 +1,12 @@
+"""Exceptions to be used in discord object lookup"""
+
+from discord import DiscordException
+
+class GuildLookupException(DiscordException):
+    """Caught to handle exceptions from discord.Client.get_guild()"""
+
+class RoleLookupException(DiscordException):
+    """Caught to handle exceptions from discord.guild.get_role()"""
+
+class ChannelLookupException(DiscordException):
+    """Caught to handle exceptions from discord.Client.get_channel()"""
